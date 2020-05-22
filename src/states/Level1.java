38389476a3046592c4c94ce0f76d98932e6ea51a
IsmailElShinnawy <ismailelshennawy@gmail.com>
2020-05-22 02:40:48 +0200

    Added reseting after pacman death

--- src/states/Level1.java
+++ src/states/Level1.java
@@ -18,10 +18,7 @@ import maps.Map;
 public class Level1 extends State implements PlayerListener {
 
 	private Map map;
-	private static final String fileName = "res/levels/level1.txt";
-
-	// private AudioPlayer beginning;
-	// private AudioPlayer intermission;
+	private static final String fileName = "/levels/level1.txt";
 
 	private boolean chaseMode = false;
 	private int chaseTimer = 0;
@@ -35,19 +32,43 @@ public class Level1 extends State implements PlayerListener {
 	private int frightenedTimer = 0;
 	private int frightenedTime = 7000;
 	private int frightenedFade = 2000;
+	
+	private boolean visPath = false;
 
 	private long now;
 	private long lastTime = System.currentTimeMillis();
+	
+	private int[] dotCount = {30, 90};
+	private final int INKY = 0, CLYDE = 1;
+	private int dots = 0;
+	private boolean spawnInky = false;
+	private boolean spawnClyde = false;
 
 	public Level1(GameStateManager gsm) {
 		map = new Map(31, 28, fileName, Assets.map);
 		addPlayerAndGhosts(new Player(14 * 16, 26 * 16, 16, map, gsm));
-
-		// beginning = new AudioPlayer("/sounds/pacman_beginning.wav");
-		// intermission = new AudioPlayer("/sounds/pacman_intermission.wav");
+	}
+	
+	public void init() {
 	}
 
-	public void init() {
+	public void resetLevel(Player player) {
+		map = new Map(31, 28, fileName, Assets.map);
+		scatterMode = true;
+		chaseMode = false;
+		frightenedMode = false;
+		
+		scatterTimer = 0;
+		chaseTimer = 0;
+		frightenedTimer = 0;
+		
+		dots = 0;
+		spawnInky = false;
+		spawnClyde = false;
+		
+		player.setMap(map);
+		
+		addPlayerAndGhosts(player);
 	}
 
 	public void tick() {
@@ -68,14 +89,27 @@ public class Level1 extends State implements PlayerListener {
 							Point pinkyTarget = getPinkyTarget();
 							g.setTarget(pinkyTarget);
 						} else if (g instanceof Clyde) {
-							g.setTarget(getPlayer().getPlayerTarget());
-							if(g.getPath().size()<=8) {
-								g.setTarget(g.getCorner());
-							} else {;
-								g.setTarget(getPlayer().getPlayerTarget());
+							Clyde clyde = (Clyde)g;
+							double dist = getEuclideanDistance(
+									g.getX(), 
+									g.getY(),
+									getPlayer().getX(),
+									getPlayer().getY()
+									);
+							if(!clyde.isShy()) {
+								if (dist <= clyde.getShyDistance()*g.getSize()) {
+									clyde.setShy(true);
+								} else {
+									g.setTarget(getPlayer().getPlayerTarget());
+								}
 							}
 						} else if (g instanceof Inky) {
-
+							Point p = getInkyTarget();
+							if(map.getNodes()[p.y][p.x].isWall()) {
+								g.setTarget(g.getCorner()); 
+							}else {
+								g.setTarget(p);
+							}
 						}
 					}
 				}
@@ -144,16 +178,78 @@ public class Level1 extends State implements PlayerListener {
 		setPlayer(player);
 		getPlayer().setListener(this);
 		addGhost(new Blinky(13 * 16, 14 * 16, 16, map, new Point(1, 1), new Point(6, 5)));
-		addGhost(new Pinky(11 * 16, 14 * 16, 16, map, new Point(26, 1), new Point(21, 5)));
-		// addEntity(new Inky(14 * 16, 17 * 16, 16, map, ));
-		// addGhost(new Clyde(16 * 16, 17 * 16, 16, map, new Point(26, 29), new
-		// Point(19, 23)));
-		addGhost(new Clyde(13 * 16, 14 * 16, 16, map, new Point(26, 29), new Point(18, 26)));
+		addGhost(new Pinky(11 * 16, 14 * 16, 16, map, new Point(26, 1), new Point(21,5)));
+		if(spawnInky) {
+			addGhost(new Inky(13 * 16, 14 * 16, 16, map, new Point(26, 29), new Point(18, 26)));
+		}
+		if(spawnClyde) {
+			addGhost(new Clyde(13 * 16, 14 * 16, 16, map, new Point(1, 29), new Point(9,26)));
+		}
+		
+		for(Ghost g: getGhosts()) {
+			g.setVisPath(visPath);
+		}
 	}
 
 	public Point getPinkyTarget() {
 		map.resetNodesForDFS();
 		return getPlayer().dfs(4);
+	}
+
+	public Point getInkyTarget() {
+		Point p = (Point) getPlayer().getPlayerTarget().clone();
+		int vx = getPlayer().getVX();
+		int vy = getPlayer().getVY();
+		Point v;
+		if (vy < 0) {
+			v = new Point(-1, -1);
+			if (p.x > 0 && p.y > 0 && !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+			if (p.x > 0 && p.y > 0 && !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+		} else if (vy > 0) {
+			v = new Point(1, 1);
+			if (p.x < map.getNodes()[p.y].length - 1 && p.y < map.getNodes().length - 1 
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+			if (p.x < map.getNodes()[p.y].length - 1 && p.y < map.getNodes().length - 1
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+		} else if (vx > 0) {
+			v = new Point(1, -1);
+			if (p.x < map.getNodes()[p.y].length - 1 && p.y > 0
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+			if (p.x < map.getNodes()[p.y].length - 1 && p.y > 0
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+		} else {
+			v = new Point(-1, 1);
+			if (p.x > 0 && p.y < map.getNodes().length - 1
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+			if (p.x > 0 && p.y < map.getNodes().length - 1
+					&& !map.getNodes()[p.y+v.y][p.x+v.x].isWall()) {
+				p.x += v.x;
+				p.y += v.y;
+			}
+		}
+
+		return p;
 	}
 
 	public void onPlayerDeath() {
@@ -175,7 +271,12 @@ public class Level1 extends State implements PlayerListener {
 		chaseTimer = 0;
 		frightenedTimer = 0;
 		getPlayer().resetForRevive();
-		addPlayerAndGhosts(getPlayer());
+		if(getPlayer().getRemainingLives()!=0) {
+			addPlayerAndGhosts(getPlayer());
+		}else {
+			getPlayer().reset();
+			resetLevel(getPlayer());;
+		}
 	}
 
 	public void onEatingEnergizer() {
@@ -185,6 +286,27 @@ public class Level1 extends State implements PlayerListener {
 			g.reverseDirection();
 			g.setFrightened(true);
 			g.setCurrentAnimation(Ghost.FRIGHTENED);
+		}
+	}
+
+	public void onSpaceBarPress() {
+		visPath = !visPath;
+		for(Ghost g: getGhosts()) {
+			g.setVisPath(visPath);
+		}
+	}
+	
+	public void onEatingDot() {
+		dots++;
+		if(dots>=dotCount[INKY] && !spawnInky) {
+			spawnInky = true;
+			addGhost(new Inky(13 * 16, 14 * 16, 16, map, new Point(26, 29), new Point(18, 26)));
+			getGhosts().get(getGhosts().size()-1).setVisPath(visPath);
+		}
+		if(dots>=dotCount[CLYDE] && !spawnClyde) {
+			spawnClyde = true;
+			addGhost(new Clyde(13 * 16, 14 * 16, 16, map, new Point(1, 29), new Point(9,26)));
+			getGhosts().get(getGhosts().size()-1).setVisPath(visPath);
 		}
 	}
 
